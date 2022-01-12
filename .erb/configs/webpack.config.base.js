d1df00bbf33d107b578122b11095ba003db276f8
Ivo Iliev <ivo.iliev@vodafone.com>
2022-01-12 23:34:31 +0000

    define variables using DefinePlugin

--- .erb/configs/webpack.config.base.js
+++ .erb/configs/webpack.config.base.js
@@ -43,11 +43,14 @@ export default {
   },
 
   plugins: [
-    new Dotenv(),
-    new webpack.EnvironmentPlugin([
-      'REACT_TODO_API_IVO',
-      'REACT_TODO_API_MARGI',
-      'REACT_APP_FORECAST_APP_ID',
-    ]),
+    new webpack.DefinePlugin({
+      'process.env': {
+        REACT_TODO_API_IVO: JSON.stringify(process.env.REACT_TODO_API_IVO),
+        REACT_TODO_API_MARGI: JSON.stringify(process.env.REACT_TODO_API_MARGI),
+        REACT_APP_FORECAST_APP_ID: JSON.stringify(
+          process.env.REACT_APP_FORECAST_APP_ID
+        ),
+      },
+    }),
   ],
 };
